e840fc6d0a4dd69280b8423b2b6859a4cc56f124
unknown <ypollo20@gmail.com>
2026-05-30 16:43:55 -0600

    Subo stack.h

--- include/stack.h
+++ include/stack.h
@@ -1,6 +1,9 @@
 #ifndef STACK_H
 #define STACK_H
 
+/* Pila genérica de enteros (PIDs)
+   Uso: historial de ejecución, backtracking */
+
 typedef struct Stack Stack;
 
 Stack* stack_create();
@@ -8,6 +11,8 @@ void stack_push(Stack* stack, int value);
 int stack_pop(Stack* stack);
 int stack_peek(Stack* stack);
 int stack_is_empty(Stack* stack);
+int stack_size(Stack* stack);
+void stack_print(Stack* stack);
 void stack_destroy(Stack* stack);
 
-#endif
+#endif 
