174549d9fb3f777b691f59949f4d2f3db8a08d85
unknown <ypollo20@gmail.com>
2026-05-30 17:57:09 -0600

    Subo test_circular_queue.c

--- tests/test_circular_queue.c
+++ tests/test_circular_queue.c
@@ -1,20 +1,29 @@
+#include <stdio.h>
 #include <assert.h>
 #include "circular_queue.h"
 
 void test_round_robin_behavior() {
-    CircularQueue* q = cq_create(3);
+    CircularQueue* cq = cq_create(4);
+    cq_enqueue(cq, 1);
+    cq_enqueue(cq, 2);
+    cq_enqueue(cq, 3);
+    assert(cq_dequeue(cq) == 1);
+    cq_enqueue(cq, 4);              /* Reinsertar al final */
+    assert(cq_dequeue(cq) == 2);
+    assert(cq_dequeue(cq) == 3);
+    assert(cq_dequeue(cq) == 4);
+    assert(cq_is_empty(cq) == 1);
+    cq_destroy(cq);
+    printf("  [OK] test_round_robin_behavior\n");
+}
 
-    cq_enqueue(q, 1);
-    cq_enqueue(q, 2);
-    cq_enqueue(q, 3);
-
-    assert(cq_dequeue(q) == 1);
-
-    cq_enqueue(q, 4);
-
-    assert(cq_dequeue(q) == 2);
-    assert(cq_dequeue(q) == 3);
-    assert(cq_dequeue(q) == 4);
-
-    cq_destroy(q);
+void test_circular_resize() {
+    CircularQueue* cq = cq_create(2);
+    cq_enqueue(cq, 10);
+    cq_enqueue(cq, 20);
+    cq_enqueue(cq, 30);  /* Debe redimensionar */
+    assert(cq_size(cq) == 3);
+    assert(cq_dequeue(cq) == 10);
+    cq_destroy(cq);
+    printf("  [OK] test_circular_resize\n");
 }
