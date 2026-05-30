89911ee110d865ce664da644d59985dbbbc6b3d0
unknown <ypollo20@gmail.com>
2026-05-30 16:59:10 -0600

    Subo linked.h

---
+++ include/linked.h
@@ -0,0 +1,25 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+/* Lista ligada simple ordenada por PID
+   Uso: almacenar procesos terminados ordenados */
+
+typedef struct LLNode {
+    int pid;
+    int burst_time;
+    struct LLNode* next;
+} LLNode;
+
+typedef struct LinkedList {
+    LLNode* head;
+    int size;
+} LinkedList;
+
+LinkedList* ll_create();
+void        ll_insert_sorted(LinkedList* list, int pid, int burst_time);
+int         ll_remove(LinkedList* list, int pid);
+LLNode*     ll_find(LinkedList* list, int pid);
+void        ll_print(LinkedList* list);
+void        ll_destroy(LinkedList* list);
+
+#endif
