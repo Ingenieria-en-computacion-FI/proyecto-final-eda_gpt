17a627e9915ee34afd9a4235e7ec261cf073d599
unknown <ypollo20@gmail.com>
2026-05-30 18:17:20 -0600

    Subo correjido stack.h

--- src/stack.h
+++ src/stack.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "stack.h"
+#include "../include/stack.h"
 
 struct Node {
     int value;
@@ -12,7 +12,7 @@ struct Stack {
 };
 
 Stack* stack_create() {
-    Stack* stack = malloc(sizeof(Stack));
+    Stack* stack = (Stack*)malloc(sizeof(Stack));
     stack->top = NULL;
     return stack;
 }
