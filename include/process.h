5bb71e861f190846f9f549a835cfdaf22d892ed0
unknown <ypollo20@gmail.com>
2026-06-01 18:12:25 -0600

    Subo process.h

--- include/process.h
+++ include/process.h
@@ -11,20 +11,20 @@ para que el código sea legible.
 typedef enum {
     READY,
     RUNNING,
-    BLOCKED,
+    BLOCKED, // si no hay memoria
     FINISHED
 } ProcessState;
 
 typedef struct {
     int pid;
-    int burst_time;
-    int remaining_time;
+    int burst_time; // tiempo total que necesita para ejecutarse
+    int remaining_time; // tiempo que le queda para terminar (se actualiza al ejecutar)
     int priority;
     int memory_required;
     ProcessState state;
 } Process;
 
 Process process_create(int pid, int burst_time, int priority, int memory_required);
-const char* process_state_str(ProcessState state);
+const char* process_state_str(ProcessState state); // Convierte el estado a string para imprimirlo
 
 #endif 
